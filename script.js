d76bcc5380160c92dd9af290acbc548d1f87f3e4
Szymon Grabowski <szmiko@wp.pl>
2018-05-07 18:47:35 +0200

    Fixed script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	console.log('DOM ready');
+  console.log('DOM ready');
 
   function randomString() {
     var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
@@ -24,22 +24,22 @@ $(document).ready(function(){
     	var $columnDelete = $('<button>').addClass('btn-delete col-md-1').text('x');
 		  var $columnAddCard = $('<button>').addClass('add-card col-md-2').text('Add a card');
 
-		$columnDelete.on('click', function() {
-      self.removeColumn();
-      alert('Column deleted.');
-		});
+		  $columnDelete.on('click', function() {
+        self.removeColumn();
+        alert('Column deleted.');
+		  });
 
-    $columnAddCard.on('click', function() {
-      self.addCard(new Card(prompt("Enter the name of the card")));
-		});
+      $columnAddCard.on('click', function() {
+        self.addCard(new Card(prompt("Enter the name of the card")));
+		  });
 
-		$column.append($columnTitle)
+		  $column.append($columnTitle)
       .append($columnDelete)
       .append($columnAddCard)
       .append($columnCardList);
-		return $column;
+		  return $column;
     };
-};
+  };
 
   Column.prototype = {
     addCard: function(card) {
@@ -51,24 +51,24 @@ $(document).ready(function(){
   };
 
   function Card(description) {
-	 var self = this;
+    var self = this;
 
-  this.id = randomString();
-  this.description = description;
-  this.$element = createCard();
+    this.id = randomString();
+    this.description = description;
+    this.$element = createCard();
 
-  function createCard() {
-    var $card = $('<li>').addClass('card');
-    var $cardDescription = $('<p>').addClass('card-description').text(self.description);
-    var $cardDelete = $('<button>').addClass('btn-delete').text('x');
+    function createCard() {
+      var $card = $('<li>').addClass('card');
+      var $cardDescription = $('<p>').addClass('card-description').text(self.description);
+      var $cardDelete = $('<button>').addClass('btn-delete').text('x');
 	
-    $cardDelete.on('click', function(){
-      self.removeCard();
-    });
+      $cardDelete.on('click', function(){
+        self.removeCard();
+      });
 
-    $card.append($cardDelete)
-		.append($cardDescription);
-		return $card;
+      $card.append($cardDelete)
+		   .append($cardDescription);
+		  return $card;
     };
   };
 
